4fb0f50ffb8267aaa90cf1ffe50d81bb6bf3bf83
milimoe <mili@wrss.org>
2024-03-21 00:41:58 +0800

    release

--- src/Settings/OSMCore.cs
+++ src/Settings/OSMCore.cs
@@ -3,7 +3,7 @@
     public class OSMCore
     {
         public const string version = "v1.0";
-        public const string version2 = "EA Beta6";
+        public const string version2 = "Release";
         public const string time = "Mar. 21st, 2024";
 
         public static string Info => $"OSM Core {version} {version2}\r\nAuthor: Milimoe\r\nBuilt on {time}\r\nSee: https://github.com/milimoe";
