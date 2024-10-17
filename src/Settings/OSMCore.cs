a960d3cf0873321deb84e9b9de085d6206195ca4
milimoe <mili@wrss.org>
2024-10-18 01:36:35 +0800

    set version

--- src/Settings/OSMCore.cs
+++ src/Settings/OSMCore.cs
@@ -6,7 +6,7 @@ namespace Milimoe.RainBOT.Settings
     public class OSMCore
     {
         public const string version = "v1.0";
-        public const string version2 = "Patch11 Test1";
+        public const string version2 = "Patch11";
 
         public static string Info => $"OSM Core {version} {version2}\r\nAuthor: Milimoe\r\nBuilt on {GetBuiltTime(Assembly.GetExecutingAssembly().Location)}\r\nSee: https://github.com/milimoe";
 
